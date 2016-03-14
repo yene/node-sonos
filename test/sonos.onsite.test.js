8dd48e2042d63f9db59108aa0f1e5723d7a55e4f
Ben Evans <ben@bensbit.co.uk>
2016-03-14 14:07:27 +0000

    Onsite Tests for Get/Set Volume

--- test/sonos.onsite.test.js
+++ test/sonos.onsite.test.js
@@ -1,18 +1,33 @@
 /* eslint-env mocha */
 const sonos = require('../')
+const assert = require('assert')
 
 describe('Search Music Library', function () {
   var device = null
 
   before(function (done) {
-    sonos.search(function (dev) {
+    var search = sonos.search()
+    search.once('DeviceAvailable', function (dev) {
       device = dev
       done()
     })
   })
 
-  it('returns search results from the Sonos library', function (done) {
-    // TODO: Verify data response
-    device.searchMusicLibrary('tracks', 'Newton', {}, done)
+  it('get/set volume', function (done) {
+    device.getVolume(function (err, initialVolume) {
+      if (err) return done(err)
+      var volumeToSet = initialVolume === 0 ? 1 : 0
+      device.setVolume(volumeToSet, function (err) {
+        if (err) return done(err)
+        device.getVolume(function (err, currentVolume) {
+          assert.equal(volumeToSet, currentVolume)
+          done()
+        })
+      })
+    })
   })
+  // it('returns search results from the Sonos library', function (done) {
+  //   // TODO: Verify data response
+  //   device.searchMusicLibrary('tracks', 'Newton', {}, done)
+  // })
 })
